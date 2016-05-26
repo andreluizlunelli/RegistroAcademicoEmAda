a38cf96ae8bb437ccf522d5a3b68e41381967655
André Lunelli <to.lunelli@gmail.com>
2016-05-25 21:27:09 -0300

    incrementando e imprimindo

--- src/main.adb
+++ src/main.adb
@@ -1,13 +1,18 @@
 with Disciplina_Crud; use Disciplina_Crud;
---with Ada.Text_IO.Integer_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
    D : Disciplina;
+   Int_Test : Integer;
 begin
- --  D := Incrementa(D);
+   D := Incrementa(D);
+   Int_Test := D.Codigo;
+--   Int_Test := Integer'Value(Int_Test);
+   Put_Line(Item => Integer'Image(Int_Test));
+
    --Put(D.Codigo);
 --   Ada.Text_IO.Integer_IO.Put(D.Codigo);
  --   Disciplina_Crud.Incrementa;
- Ada.Text_IO.Put_Line("opa");
+
 end Main;
