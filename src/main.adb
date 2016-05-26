879df9588a72e317684ce98d21532ac770876fad
André Lunelli <to.lunelli@gmail.com>
2016-05-25 21:09:01 -0300

    correcao .gitignore

--- src/main.adb
+++ src/main.adb
@@ -5,8 +5,9 @@ with Ada.Text_IO; use Ada.Text_IO;
 procedure Main is
    D : Disciplina;
 begin
-   D := Incrementa(D);
-   Put(""+D.Codigo);
+ --  D := Incrementa(D);
+   --Put(D.Codigo);
 --   Ada.Text_IO.Integer_IO.Put(D.Codigo);
---   Disciplina_Crud.Incrementa;
+ --   Disciplina_Crud.Incrementa;
+ Ada.Text_IO.Put_Line("opa");
 end Main;
